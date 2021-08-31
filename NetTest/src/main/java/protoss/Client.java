17ed2ee576097eb3d534a089886f5bce1e81072b
Protoss <759872054@qq.com>
2021-09-01 00:36:57 +0800

    bio

---
+++ NetTest/src/main/java/protoss/Client.java
@@ -0,0 +1,37 @@
+package protoss;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("127.0.0.1", 8888);
+        OutputStream outputStream = socket.getOutputStream();
+        InputStream in = socket.getInputStream();
+        new Thread(() -> {
+            //读取
+            byte[] bytes = new byte[1024];
+            try {
+                while (in.read(bytes) != -1) {
+                    System.out.println(new String(bytes));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        while (true) {
+            //发送
+            Scanner scanner = new Scanner(System.in);
+            final String s = scanner.next();
+            try {
+                outputStream.write(s.getBytes());
+                outputStream.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
---
+++ NetTest/src/main/java/protoss/Server.java
@@ -0,0 +1,41 @@
+package protoss;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        ServerSocket server = new ServerSocket();
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(5, 10, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10));
+        server.bind(new InetSocketAddress("127.0.0.1", 8888));
+        while (true) {
+            Socket socket = server.accept();
+            poolExecutor.submit(() -> {
+
+            });
+        }
+    }
+
+    private void receive(Socket socket) throws Exception {
+        InputStream in = socket.getInputStream();
+        //读取
+        String s;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        while ((s = reader.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+
+    private void send(Socket socket) throws Exception {
+        OutputStream os = socket.getOutputStream();
+        //发送
+        os.write("received！".getBytes());
+        os.flush();
+        System.out.println("发送！");
+    }
+}
