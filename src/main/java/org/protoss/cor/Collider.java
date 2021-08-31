a68e5129b0d7991594ea6143c9afa531ff5577ab
Protoss <759872054@qq.com>
2021-08-31 20:13:26 +0800

    1.collider注释

--- src/main/java/org/protoss/cor/Collider.java
+++ src/main/java/org/protoss/cor/Collider.java
@@ -6,5 +6,11 @@ import org.protoss.GameObject;
  * 碰撞接口
  */
 public interface Collider {
+    /**
+     * 碰撞检测
+     * @param o1 游戏物体1
+     * @param o2 游戏物体2
+     * @return 是否调用下一个责任链结点
+     */
     boolean collide(GameObject o1, GameObject o2);
 }
